27da5dc75e833ccd60af895ac903a31941721cee
Vsevolod Scovitin <scovitin.vsevolod@gmail.com>
2019-06-19 13:58:12 +0300

    HW20-21 HWC20 Article development

---
+++ docroot/sites/all/modules/hwc/hwc_homepage/templates/hwc_footer_section.tpl.php
@@ -0,0 +1,24 @@
+<?php
+$skip = variable_get('footer_section_skip_empty', FALSE);
+$exclude_url = explode("\n", $skip);
+?>
+<div class="footer-section hidden-xs">
+  <?php foreach ($data as $menu) {
+    if ((!$menu['below'] && $skip) || (in_array($menu['link']['href'], $exclude_url))) {
+      continue;
+    }
+    ?>
+      <div class="col-md-2">
+          <?php print $menu['link']['title']; ?>
+          <ul>
+            <?php foreach ($menu['below'] as $submenu) {
+              if (in_array($submenu['link']['href'], $exclude_url)) {
+                continue;
+              }
+              ?>
+                <li><?php print l($submenu['link']['title'], $submenu['link']['href']); ?></li>
+            <?php } ?>
+          </ul>
+      </div>
+  <?php } ?>
+</div>
