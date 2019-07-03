66e36db6c9463edf7684f8e75014598c3131e2b2
Vsevolod Scovitin <scovitin.vsevolod@gmail.com>
2019-07-03 12:04:08 +0300

    HW20-28 HWC20 General search development

--- docroot/sites/all/modules/osha/osha_search/sort.js
+++ docroot/sites/all/modules/osha/osha_search/sort.js
@@ -8,15 +8,15 @@
 
     Drupal.behaviors.search_sort = {
         attach: function(context, settings) {
-            jQuery('#edit-sort-by').change(function(){
-                jQuery('#views-exposed-form-search-site #edit-sort-by--2').val(jQuery(this).val());
-                jQuery('#views-exposed-form-search-site').submit();
-            });
-            if (jQuery('#osha-search-sort-form').length) {
-              document.getElementById('osha-search-sort-form').onsubmit = function() {
-                return false;
-              };
-            }
+            // jQuery('#edit-sort-by').change(function(){
+            //     jQuery('#views-exposed-form-search-site #edit-sort-by--2').val(jQuery(this).val());
+            //     jQuery('#views-exposed-form-search-site').submit();
+            // });
+            // if (jQuery('#osha-search-sort-form').length) {
+            //   document.getElementById('osha-search-sort-form').onsubmit = function() {
+            //     return false;
+            //   };
+            // }
         }
     }
 })(jQuery);
