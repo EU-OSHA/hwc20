cae204f0b1d5c224920fcab7cd3c134db53947b9
Vsevolod Scovitin <scovitin.vsevolod@gmail.com>
2019-08-30 13:16:53 +0300

    HW20-46 HWC20 Development -Events list, detail, backend

--- docroot/sites/all/themes/hwc_frontend/template.php
+++ docroot/sites/all/themes/hwc_frontend/template.php
@@ -268,13 +268,13 @@ function hwc_frontend_preprocess_page(&$vars) {
   $vars['back_to_pz'] = hwc_partner_back_to_private_zone();
   $vars['page']['content']['#post_render'] = ['hwc_content_post_render'];
   // Change Events page title.
-  if (!empty($vars['theme_hook_suggestions']['1']) && in_array($vars['theme_hook_suggestions']['1'],
-      array('page__media_centre__events', 'page__media_centre__past_events'))) {
-    $title = '<span id="block-osha-events-events-links">';
-    $title .= l(t('Upcoming events'), 'media-centre/events') . ' / ' . l(t('Past events'), 'media-centre/past-events');
-    $title .= '</span>';
-    drupal_set_title($title, PASS_THROUGH);
-  }
+//  if (!empty($vars['theme_hook_suggestions']['1']) && in_array($vars['theme_hook_suggestions']['1'],
+//      array('page__media_centre__events', 'page__media_centre__past_events'))) {
+//    $title = '<span id="block-osha-events-events-links">';
+//    $title .= l(t('Upcoming events'), 'media-centre/events') . ' / ' . l(t('Past events'), 'media-centre/past-events');
+//    $title .= '</span>';
+//    drupal_set_title($title, PASS_THROUGH);
+//  }
   if (drupal_is_front_page()) {
     unset($vars['page']['content']['system_main']['default_message']);
     drupal_set_title('');
@@ -396,29 +396,12 @@ function hwc_frontend_preprocess_page(&$vars) {
         $breadcrumb = array();
         $breadcrumb[] = l(t('Home'), '<front>');
         $breadcrumb[] = t('Media centre');
-
-        if ($date < $now) {
-          $breadcrumb[] = l(t('Past events'), 'media-centre/past-events');
-
-          $link_title = t('Back to past events list');
-          $link_href = 'media-centre/past-events';
-          $tag_vars['element']['#value'] = t('Past events');
-          $vars['page']['above_title']['events-page-title'] = array(
-            '#type' => 'item',
-            '#markup' => theme('html_tag', $tag_vars),
-          );
-        }
-        else {
-          $breadcrumb[] = l(t('Upcoming events'), 'media-centre/events');
-
-          $link_title = t('Back to events list');
-          $link_href = 'media-centre/events';
-          $tag_vars['element']['#value'] = t('Upcoming events');
-          $vars['page']['above_title']['practical-tool-page-title'] = array(
-            '#type' => 'item',
-            '#markup' => theme('html_tag', $tag_vars),
-          );
-        }
+        $breadcrumb[] = l(t('Events'), 'media-centre/events');
+        $tag_vars['element']['#value'] = t('Events');
+        $vars['page']['above_title']['events-page-title'] = array(
+          '#type' => 'item',
+          '#markup' => theme('html_tag', $tag_vars),
+        );
         $breadcrumb[] = $node->title;
         drupal_set_breadcrumb($breadcrumb);
 
