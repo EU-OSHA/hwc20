a6dd0845c7a4b4e1e1b956ae1833901be642c46f
iariasbm <[iarias@bilbomatica.es]>
2019-10-18 14:37:05 +0200

    HW20-209 - HWC20 Development of the complete GPA

--- docroot/sites/all/themes/hwc_frontend/templates/block--gpexamples.tpl.php
+++ docroot/sites/all/themes/hwc_frontend/templates/block--gpexamples.tpl.php
@@ -1,7 +1,7 @@
 <?php
 $translated = osha_tmgmt_literal_get_translation('Good practice examples');
 ?>
-<section class="container slider--video--section">
+<section class="slider--video--section">
   <div class="slider--video--block row">
     <div class="slider--video--items">
       <h2 class="block-title"><?php print $translated; ?></h2>
