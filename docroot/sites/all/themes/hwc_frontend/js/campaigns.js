ddc87b7157577b97ed86de12558a61ec8ebc605c
Vsevolod Scovitin <scovitin.vsevolod@gmail.com>
2019-11-04 12:55:30 +0200

    merge release_1

--- docroot/sites/all/themes/hwc_frontend/js/campaigns.js
+++ docroot/sites/all/themes/hwc_frontend/js/campaigns.js
@@ -273,13 +273,6 @@ jQuery(document).ready(function() {
   		jQuery('#block-osha-newsletter-osha-newsletter-subscribe div.captcha').show();
 	});
 
-	/*changing capcha on click*/
-	jQuery("#edit-email").click(function(){
-		jQuery("#edit-submit").css({'margin-left':'auto', 'margin-right':'auto', 'display':'block'});
-		jQuery(this).css("margin-right", "22px");
-		jQuery(".form-item-email").css({"margin-right":"auto", "margin-left":"auto", "display":"table"});
-	});
-
 	/*adding color to "sort by" labels when is checked*/
 	jQuery(".pane-hwc-practical-tool-hwc-practical-tool-listing #edit-content .form-type-radios.form-item-sort input:checked").parent('label').css({'color':'#749b00','font-weight':'bold'});
 
