7cf63eefb4f728c3fd2c99a1d1b64d0296408ad5
Vsevolod Scovitin <scovitin.vsevolod@gmail.com>
2019-11-05 18:42:34 +0200

    Events searching using date ranges

--- docroot/sites/all/modules/osha/osha_events/js/filter_dates.js
+++ docroot/sites/all/modules/osha/osha_events/js/filter_dates.js
@@ -1,13 +1,23 @@
 (function ($) {
   Drupal.behaviors.search_sort = {
     attach: function (context, settings) {
+      jQuery('#edit-years').change(function () {
+        if (jQuery(this).val()) {
+          jQuery('#edit-field-start-date-value-datepicker-popup-0').val();
+          jQuery('#edit-field-start-date-value2-datepicker-popup-0').val();
+          jQuery('#edit-from-date-datepicker-popup-0').val();
+          jQuery('#edit-to-date-datepicker-popup-0').val();
+        }
+      });
       jQuery('#edit-from-date-datepicker-popup-0').change(function () {
         jQuery('#edit-field-start-date-value-datepicker-popup-0').val(jQuery(this).val());
         jQuery('#views-exposed-form-events-search-page').submit();
+        jQuery('#edit-years').val();
       });
       jQuery('#edit-to-date-datepicker-popup-0').change(function () {
         jQuery('#edit-field-start-date-value2-datepicker-popup-0').val(jQuery(this).val());
         jQuery('#views-exposed-form-events-search-page').submit();
+        jQuery('#edit-years').val();
       });
       jQuery('#edit-years').change(function () {
         jQuery('#edit-field-start-date-value-datepicker-popup-0').val('');
