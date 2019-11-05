db92769c6bba39f873a5c10ef9a54b8cd0a6ca29
Vsevolod Scovitin <scovitin.vsevolod@gmail.com>
2019-11-05 17:53:41 +0200

    Release 1 + Release 2 - Campaign materials detail page

--- docroot/sites/all/themes/hwc_frontend/template.php
+++ docroot/sites/all/themes/hwc_frontend/template.php
@@ -311,9 +311,6 @@ function hwc_frontend_preprocess_page(&$vars) {
           $vars['theme_hook_suggestions'][] = 'page__european__week';
           $vars['title_suffix'] = '<div id="european_week_date">' . variable_get('european_week_date', '12<sup>th</sup>-14<sup>th</sup> of October 2020') . '</div>';
         }
-        elseif ($n->nid == 163) {
-          $vars['theme_hook_suggestions'][] = 'page__gpa';
-        }
         else {
           $vars['theme_hook_suggestions'][] = 'page__node__article';
         }
