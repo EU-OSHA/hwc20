9a79e9f992bf4ea18416a16ade7eb86c36945bac
Vsevolod Scovitin <scovitin.vsevolod@gmail.com>
2020-01-07 11:48:13 +0200

    About the topic menu changes

--- docroot/sites/all/themes/hwc_frontend/templates/block--menu_block--4.tpl.php
+++ docroot/sites/all/themes/hwc_frontend/templates/block--menu_block--4.tpl.php
@@ -6,8 +6,7 @@
  * Complete documentation for this file is available online.
  * @see https://drupal.org/node/1728246
  */
-?>
-<?php
+$translated = osha_tmgmt_literal_get_translation($title);
 $node = menu_get_object();
 if (isset($node) && isset($node->field_article_type) && $node->field_article_type[LANGUAGE_NONE][0]['tid'] == HWC_INTRODUCTION_ARTICLE) {
 }
@@ -16,7 +15,7 @@ else {
     <div id="<?php print $block_html_id; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
       <?php print render($title_prefix); ?>
       <?php if ($translated): ?>
-        <h2<?php print $title_attributes; ?>><?php print $translated; ?></h2>
+        <h2<?php print $title_attributes; ?>><?php print l($translated, variable_get('about_topic_path','about-topic')); ?></h2>
       <?php endif; ?>
       <?php print render($title_suffix); ?>
 
--- docroot/sites/all/themes/hwc_frontend/templates/block--menu_block--6.tpl.php
+++ docroot/sites/all/themes/hwc_frontend/templates/block--menu_block--6.tpl.php
@@ -7,15 +7,13 @@
  * @see https://drupal.org/node/1728246
  *
  */
-?>
-<?php
 $translated = osha_tmgmt_literal_get_translation($title);
 ?>
 <div id="<?php print $block_html_id; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
 
   <?php print render($title_prefix); ?>
   <?php if ($translated): ?>
-    <h2<?php print $title_attributes; ?>><?php print $translated; ?></h2>
+    <h2<?php print $title_attributes; ?>><?php print l($translated, variable_get('about_topic_path','about-topic')); ?></h2>
   <?php endif; ?>
   <?php print render($title_suffix); ?>
 
