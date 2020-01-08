56a37bc83ce056ea9fea12f70ebca0f705d037cc
Vsevolod Scovitin <scovitin.vsevolod@gmail.com>
2020-01-08 13:09:23 +0200

    HW20-422 Get your certificate: The certificate sent in the email is not HWC 2022 certificate

--- docroot/sites/all/modules/hwc/hwc/templates/hwc_html_mail_footer.tpl.php
+++ docroot/sites/all/modules/hwc/hwc/templates/hwc_html_mail_footer.tpl.php
@@ -13,60 +13,68 @@ $bg_path = $base_url . '/sites/all/modules/osha/osha_newsletter/images/social-ba
       <table border="0" cellpadding="0" cellspacing="0" width="100%">
         <tbody>
         <tr class="social footer-social" style="background: url(<?php echo $bg_path; ?>) no-repeat; height: 100px;">
-          <td style="width:150px;">
-            &nbsp;
-          </td>
-          <?php
-          $social = array(
-            'email-twitter' => array(
-              'path' => 'https://twitter.com/eu_osha',
-              'alt' => t('Twitter'),
-              'width' => 31,
-            ),
-            'email-facebook' => array(
-              'path' => 'https://www.facebook.com/EuropeanAgencyforSafetyandHealthatWork',
-              'alt' => t('Facebook'),
-              'width' => 25,
-            ),
-            'email-linkedin' => array(
-              'path' => 'https://www.linkedin.com/company/european-agency-for-safety-and-health-at-work',
-              'alt' => t('LinkedIn'),
-              'width' => 25,
-            ),
-            'email-youtube' => array(
-              'path' => 'https://www.youtube.com/user/EUOSHA',
-              'alt' => t('Youtube'),
-              'width' => 35,
-            ),
-            'email-flickr' => array(
-              'path' => 'https://www.flickr.com/photos/euosha/albums',
-              'alt' => t('Flickr'),
-              'width' => 31,
-            ),
-          );
-          foreach ($social as $name => $options) {
-            $directory = drupal_get_path('module', 'osha_newsletter');
+          <td>
+            <table border="0" cellpadding="0" cellspacing="0" width="100%">
+              <tbody>
+              <tr style="border: 0; background-color: transparent;">
+                <td style="width:150px;">
+                  &nbsp;
+                </td>
+                <?php
+                $social = array(
+                  'email-twitter' => array(
+                    'path' => 'https://twitter.com/eu_osha',
+                    'alt' => t('Twitter'),
+                    'width' => 31,
+                  ),
+                  'email-facebook' => array(
+                    'path' => 'https://www.facebook.com/EuropeanAgencyforSafetyandHealthatWork',
+                    'alt' => t('Facebook'),
+                    'width' => 25,
+                  ),
+                  'email-linkedin' => array(
+                    'path' => 'https://www.linkedin.com/company/european-agency-for-safety-and-health-at-work',
+                    'alt' => t('LinkedIn'),
+                    'width' => 25,
+                  ),
+                  'email-youtube' => array(
+                    'path' => 'https://www.youtube.com/user/EUOSHA',
+                    'alt' => t('Youtube'),
+                    'width' => 35,
+                  ),
+                  'email-flickr' => array(
+                    'path' => 'https://www.flickr.com/photos/euosha/albums',
+                    'alt' => t('Flickr'),
+                    'width' => 31,
+                  ),
+                );
+                foreach ($social as $name => $options) {
+                  $directory = drupal_get_path('module', 'osha_newsletter');
 
-            $width = $options['width'];
-            print '<td style="width: 60px; text-align: center; padding-top: 5px;">' . "\n";
-            print l(
-              theme('image', array(
-                'path' => $directory . '/images/' . $name . '.png',
-                'width' => $width,
-                'height' => 25,
-                'alt' => $options['alt'],
-                'attributes' => array('style' => 'border:0px;height:25px;max-height:25px;width: ' . $width . 'px;max-width:' . $width . 'px;', 'class' => 'social-logo'),
-              )
-            ), $options['path'], array(
-              'attributes' => array('target' => '_blank'),
-              'html' => TRUE,
-              'external' => TRUE,
-            )) . "\n";
-            print '</td>' . "\n";
-          }
-          ?>
-          <td style="width:150px;">
-            &nbsp;
+                  $width = $options['width'];
+                  print '<td style="width: 60px; text-align: center; padding-top: 5px;">' . "\n";
+                  print l(
+                      theme('image', array(
+                          'path' => $directory . '/images/' . $name . '.png',
+                          'width' => $width,
+                          'height' => 25,
+                          'alt' => $options['alt'],
+                          'attributes' => array('style' => 'border:0px;height:25px;max-height:25px;width: ' . $width . 'px;max-width:' . $width . 'px;', 'class' => 'social-logo'),
+                        )
+                      ), $options['path'], array(
+                      'attributes' => array('target' => '_blank'),
+                      'html' => TRUE,
+                      'external' => TRUE,
+                    )) . "\n";
+                  print '</td>' . "\n";
+                }
+                ?>
+                <td style="width:150px;">
+                  &nbsp;
+                </td>
+              </tr>
+              </tbody>
+            </table>
           </td>
         </tr>
         </tbody>
