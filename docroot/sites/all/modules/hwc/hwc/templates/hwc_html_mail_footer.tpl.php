6baaffe2687e5e57a0edd4ea5cc9762874bbff73
Vsevolod Scovitin <scovitin.vsevolod@gmail.com>
2020-01-08 10:37:58 +0200

    HW20-422 Get your certificate: The certificate sent in the email is not HWC 2022 certificate

--- docroot/sites/all/modules/hwc/hwc/templates/hwc_html_mail_footer.tpl.php
+++ docroot/sites/all/modules/hwc/hwc/templates/hwc_html_mail_footer.tpl.php
@@ -1,97 +1,69 @@
-<table border="0" cellpadding="0" cellspacing="0" width="100%" class="template-container newsletter-container newsletter-footer">
+<?php
+$directory = drupal_get_path('module', 'osha_newsletter');
+$unsubscribe_text = variable_list('unsubscribe_campaign_news_text', 'To unsubscribe from the CMPAIGN-NEWS list, click the following link:');
+$unsubscribe_url = variable_list('unsubscribe_campaign_news_url', 'http://list.osha.eu/scripts/wa.exe?SUBED1=CAMPAIGN-NEWS&A=1');
+?>
+<table border="0" cellpadding="0" cellspacing="0" width="100%" class="template-container newsletter-container newsletter-footer" style="max-width:800px; width:800px">
   <tbody>
-    <tr>
-      <?php
-        global $base_url;
-        $bg_path = $base_url . '/sites/all/modules/osha/osha_newsletter/images/bg-campaign-partner.png';
-      ?>
-      <td background="<?php print $bg_path; ?>" bgcolor="#416400" valign="top" style="background-image: url('<?php print $bg_path; ?>'); background-repeat: no-repeat; background-size: cover; padding: 0;">
-        <!--[if gte mso 9]>
-          <v:rect xmlns:v="urn:schemas-microsoft-com:vml" fill="true" stroke="false" style="mso-width-percent:1000;">
-            <v:fill type="frame" src="<?php print $bg_path; ?>" color="#416400" />
-            <v:textbox style="mso-fit-shape-to-text:true" inset="0,0,0,0">
-        <![endif]-->
-        <div>
-          <table border="0" cellpadding="0" cellspacing="0" width="100%">
-            <tbody>
-              <tr class="social footer-social">
-                <td>
-                  <span class="hidden-mobile">
-                    Follow us on:&nbsp;&nbsp;&nbsp;&nbsp;
-                  </span>
-                  <?php
-                    $social = array(
-                      'twitter' => array(
-                        'path' => 'https://twitter.com/eu_osha',
-                        'alt' => t('Twitter'),
-                      ),
-                      'face' => array(
-                        'path' => 'https://www.facebook.com/EuropeanAgencyforSafetyandHealthatWork',
-                        'alt' => t('Facebook'),
-                      ),
-                      'linkedin' => array(
-                        'path' => 'https://www.linkedin.com/company/european-agency-for-safety-and-health-at-work',
-                        'alt' => t('LinkedIn'),
-                      ),
-                      'youtube' => array(
-                        'path' => 'https://www.youtube.com/user/EUOSHA',
-                        'alt' => t('Youtube'),
-                      ),
-                      'flickr' => array(
-                        'path' => 'https://www.flickr.com/photos/euosha/albums',
-                        'alt' => t('Flickr'),
-                      ),
-                      'blog' => array(
-                        'path' => url('tools-and-publications/blog', array(
-                          'alias' => TRUE,
-                          'absolute' => TRUE,
-                        )
-                        ),
-                        'alt' => t('Blog'),
-                      ),
-                    );
-
-                    foreach ($social as $name => $options) {
-                      $directory = drupal_get_path('module', 'osha_newsletter');
-
-                      print l(theme('image',
-                        array(
-                          'path' => $directory . '/images/' . $name . '--green.png',
-                          'width' => 20,
-                          'height' => 20,
-                          'alt' => $options['alt'],
-                          'attributes' => array('style' => 'border:0px;height:20px;max-height:20px;width: 20px;max-width:20px;', 'class' => 'social-logo'),
-                        )
-                      ), $options['path'], array(
-                        'attributes' => array('style' => 'color:#ffffff;text-decoration:none;', 'target' => '_blank'),
-                        'html' => TRUE,
-                        'external' => TRUE,
-                      ));
-
-                      print sprintf('<span class="spacing-%s">&nbsp;&nbsp;&nbsp;&nbsp;</span>', $name);
-                    }
-                  ?>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-        <!--[if gte mso 9]>
-          </v:textbox>
-        </v:rect>
-        <![endif]-->
-      </td>
-    </tr>
-
-    <tr class="footer-disclaimer">
-      <td>
-        <p><?php echo $bottom_text; ?></p>
-      </td>
-    </tr>
+  <tr>
+    <td>
+      <table border="0" cellpadding="0" cellspacing="0" width="100%">
+        <tbody>
+        <tr class="social footer-social" style="background: url(<?php echo '/' . $directory . '/images/'; ?>social-background.png) no-repeat; background-size: cover; height: 100px;">
+          <td style="width:150px;">
+            &nbsp;
+          </td>
+          <td style="color: #FFF; text-align: end; font-size: 30px; padding-right: 10px; font-family: verdana">
+            Follow us on
+          </td>
+          <td style="width: 60px; text-align: center; padding-top: 5px;">
+            <a href="https://twitter.com/eu_osha">
+              <img src="<?php echo '/' . $directory . '/images/'; ?>email-twitter.png">
+            </a>
+          </td>
+          <td style="width: 60px; text-align: center; padding-top: 5px;">
+            <a href="https://www.facebook.com/EuropeanAgencyforSafetyandHealthatWork">
+              <img src="<?php echo '/' . $directory . '/images/'; ?>email-facebook.png">
+            </a>
+          </td>
+          <td style="width: 60px; text-align: center; padding-top: 5px;">
+            <a href="https://www.linkedin.com/company/european-agency-for-safety-and-health-at-work">
+              <img src="<?php echo '/' . $directory . '/images/'; ?>email-linkedin.png">
+            </a>
+          </td>
+          <td style="width: 60px; text-align: center; padding-top: 5px;">
+            <a href="https://www.youtube.com/user/EUOSHA">
+              <img src="<?php echo '/' . $directory . '/images/'; ?>email-youtube.png">
+            </a>
+          </td>
+          <td style="width: 60px; text-align: center; padding-top: 5px;">
+            <a href="https://www.flickr.com/photos/euosha/albums">
+              <img src="<?php echo '/' . $directory . '/images/'; ?>email-flickr.png">
+            </a>
+          </td>
+          <td style="width:150px;">
+            &nbsp;
+          </td>
+        </tr>
+        </tbody>
+      </table>
+      <table style="padding-top: 15px; width: 700px; margin-left: 50px;">
+        <tr>
+          <td style="border-bottom: 1px solid #999999;padding-bottom: 20px; font-size: 10px; font-family: verdana; color: #666666; text-align: center;">
+            <?php echo $bottom_text; ?>
+          </td>
+        </tr>
+      </table>
+      <table style="padding-top: 5px; width: 700px; margin-left: 50px;">
+        <tr>
+          <td style="padding-bottom: 20px; font-size: 10px; font-family: verdana; color: #666666; text-align: center;">
+            <?php echo $unsubscribe_text; ?>
+            <br>
+            <a href="<?php echo $unsubscribe_url; ?>"><?php echo $unsubscribe_url; ?></a>
+          </td>
+        </tr>
+      </table>
+    </td>
+  </tr>
   </tbody>
 </table>
-<div class="gmailfix" style="white-space:nowrap; font:15px courier; line-height:0;">
-  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
-  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
-  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
-</div>
