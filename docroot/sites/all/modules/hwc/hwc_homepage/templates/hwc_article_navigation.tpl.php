bf3ad64e742b6e8a0d563301fc892c418592f78e
Vsevolod Scovitin <scovitin.vsevolod@gmail.com>
2020-02-26 11:59:23 +0200

    merge rel 2 changes

--- docroot/sites/all/modules/hwc/hwc_homepage/templates/hwc_article_navigation.tpl.php
+++ docroot/sites/all/modules/hwc/hwc_homepage/templates/hwc_article_navigation.tpl.php
@@ -2,30 +2,29 @@
 $theme_dir = drupal_get_path('theme', 'hwc_frontend');
 if ($prev || $next) {
   ?>
-    <div class="previous-next hidden-xs">
-      <?php  if ($prev) { ?>
-          <div class="previous">
-              <a href="<?php print url($prev['link_path']); ?>"><img src="/<?php print $theme_dir; ?>/images/previous.png"></a>
-              <div class="previous-text">
-          <a href="<?php print url($prev['link_path']); ?>">
-                  <span><?php print t('Previous');?></span>
-                  <span><?php print $prev['title'] ?></span>
-          </a>
-              </div>
-          </div>
-      <?php }?>
-      <?php  if ($next) {?>
-          <div class="next">
-              <div class="next-text">
-          <a href="<?php print url($next['link_path']); ?>">
-                  <span><?php print t('Next');?></span>
-                  <span><?php print $next['title'] ?></span>
-          </a>
-              </div>
-              <a href="<?php print url($next['link_path']); ?>"><img src="/<?php print $theme_dir; ?>/images/next.png"></a>
-          </div>
-      <?php }?>
-    </div>
+  <div class="previous-next hidden-xs">
+    <?php  if ($prev) { ?>
+      <div class="previous">
+        <a href="<?php print url($prev['link_path']); ?>">
+          <img src="/<?php print $theme_dir; ?>/images/previous.png">
+          <span class="previous-text">
+            <span><?php print t('Previous');?></span>
+            <span><?php print $prev['title'] ?></span>
+          </span>
+        </a>
+      </div>
+    <?php }?>
+    <?php  if ($next) {?>
+      <div class="next">
+        <a href="<?php print url($next['link_path']); ?>">
+          <span class="next-text">
+            <span><?php print t('Next');?></span>
+            <span><?php print $next['title'] ?></span>
+          </span>
+          <img src="/<?php print $theme_dir; ?>/images/next.png"></a>
+      </div>
+    <?php }?>
+  </div>
   <?php
 }
 ?>
