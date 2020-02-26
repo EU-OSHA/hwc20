91a004566675f77c7cb4101a028274c37b27189b
Vsevolod Scovitin <scovitin.vsevolod@gmail.com>
2020-02-26 15:23:47 +0200

    HW20-477 UX Splash Page changes - Mock up

--- docroot/sites/all/modules/hwc/hwc_homepage/templates/hwc_homepage_title_banner_countdown.tpl.php
+++ docroot/sites/all/modules/hwc/hwc_homepage/templates/hwc_homepage_title_banner_countdown.tpl.php
@@ -3,25 +3,6 @@
 </section>
 <section class="comming-soon">
   <div class="content-counter">
-    <h2><?php print t('Coming soon') ?></h2>
-    <time id="eventTime" datetime="<?php echo variable_get('splash_coming_soon_datetime', '2019-10-31T14:30:00+01:00')?>"></time>
-    <ul class="time-and-counters">
-      <li id="count-days">
-        <span class="counter-value"></span>
-        <span class="counter-label"><?php print t('days'); ?></span>
-      </li>
-      <li id="count-hours">
-        <span class="counter-value"></span>
-        <span class="counter-label"><?php print t('hours'); ?></span>
-      </li>
-      <li id="count-minutes">
-        <span class="counter-value"></span>
-        <span class="counter-label"><?php print t('minutes'); ?></span>
-      </li>
-      <li id="count-seconds">
-        <span class="counter-value"></span>
-        <span class="counter-label"><?php print t('seconds'); ?></span>
-      </li>
-    </ul>
+    <h2><?php print t(variable_get('splash_coming_text', 'European campaign launch in October 2020')) ?></h2>
   </div>
 </section>
