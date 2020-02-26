ad1feddfcb26f6de8d3b3c727066497ddfdb159a
Vsevolod Scovitin <scovitin.vsevolod@gmail.com>
2020-02-26 11:51:27 +0200

    HW20-486 UX: Depending on the level change the banner titles

--- docroot/sites/all/modules/hwc/hwc_homepage/templates/hwc_article_navigation.tpl.php
+++ docroot/sites/all/modules/hwc/hwc_homepage/templates/hwc_article_navigation.tpl.php
@@ -5,24 +5,23 @@ if ($prev || $next) {
   <div class="previous-next hidden-xs">
     <?php  if ($prev) { ?>
       <div class="previous">
-        <a href="<?php print url($prev['link_path']); ?>"><img src="/<?php print $theme_dir; ?>/images/previous.png"></a>
-        <div class="previous-text">
-          <a href="<?php print url($prev['link_path']); ?>">
+        <a href="<?php print url($prev['link_path']); ?>">
+          <img src="/<?php print $theme_dir; ?>/images/previous.png">
+          <span class="previous-text">
             <span><?php print t('Previous');?></span>
             <span><?php print $prev['title'] ?></span>
-          </a>
-        </div>
+          </span>
+        </a>
       </div>
     <?php }?>
     <?php  if ($next) {?>
       <div class="next">
-        <div class="next-text">
-          <a href="<?php print url($next['link_path']); ?>">
+        <a href="<?php print url($next['link_path']); ?>">
+          <span class="next-text">
             <span><?php print t('Next');?></span>
             <span><?php print $next['title'] ?></span>
-          </a>
-        </div>
-        <a href="<?php print url($next['link_path']); ?>"><img src="/<?php print $theme_dir; ?>/images/next.png"></a>
+          </span>
+          <img src="/<?php print $theme_dir; ?>/images/next.png"></a>
       </div>
     <?php }?>
   </div>
