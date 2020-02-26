cc72795d2cccff6801f40ee60a8227520b0cec7e
Vsevolod Scovitin <scovitin.vsevolod@gmail.com>
2020-02-26 15:22:06 +0200

    HW20-477 UX Splash Page changes - Mock up

--- docroot/sites/all/modules/hwc/hwc_homepage/js/splash-page.js
+++ docroot/sites/all/modules/hwc/hwc_homepage/js/splash-page.js
@@ -1,39 +1,17 @@
-(function($){
-    $(document).ready(function() {
-      // Counter
-      var eventTime = moment($("#eventTime").attr('datetime'));
-      var updateCounter = function(duration) {
-        $("#count-days .counter-value").text(Math.floor(duration.asDays()));
-        $("#count-hours .counter-value").text(duration.hours());
-        $("#count-minutes .counter-value").text(duration.minutes());
-        $("#count-seconds .counter-value").text(duration.seconds());
-      };
+(function($) {
+  $(document).ready(function() {
+    // Font size.
+    $("[data-font]").click(function() {
+      if (!$(this).hasClass("is_selected")) {
 
-      var currentTime = moment();
-      var diffTime = eventTime - currentTime;
-      var duration = moment.duration(diffTime, 'milliseconds');
-      var interval = 1000;
-      updateCounter(duration);
+        $("html").removeClass("smaller larger");
+        $("html").addClass($(this).data("font"));
 
-      setInterval(function(){
+        $("[data-font].is_selected").removeClass("is_selected");
+        $(this).addClass("is_selected");
 
-        duration = moment.duration(duration - interval, 'milliseconds');
-        updateCounter(duration);
-
-      }, interval);
-
-      // Font size
-      $("[data-font]").click(function() {
-        if (!$(this).hasClass("is_selected")) {
-
-          $("html").removeClass("smaller larger");
-          $("html").addClass($(this).data("font"));
-
-          $("[data-font].is_selected").removeClass("is_selected");
-          $(this).addClass("is_selected");
-
-        }
-      });
-
+      }
     });
+
+  });
 })(jQuery);
