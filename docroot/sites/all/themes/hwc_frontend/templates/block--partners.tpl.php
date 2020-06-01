21df2a0bf182111b00fc5535ff96f3f1a472665c
Vsevolod Scovitin <scovitin.vsevolod@gmail.com>
2020-06-01 14:12:44 +0300

    MHWC-47 UX II recommendations home page

--- docroot/sites/all/themes/hwc_frontend/templates/block--partners.tpl.php
+++ docroot/sites/all/themes/hwc_frontend/templates/block--partners.tpl.php
@@ -1,7 +1,7 @@
 <?php
 $translated = osha_tmgmt_literal_get_translation($title);
 ?>
-<section class="slider--video--section">
+<section class="slider--video--section container">
     <div class="slider--video--block row">
         <div class="slider--video--items">
             <h2 class="block-title"><?php print $translated; ?></h2>
