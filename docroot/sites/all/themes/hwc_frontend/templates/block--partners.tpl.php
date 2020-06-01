8ba51422bb8a370feee72f144c4a9007fb800a9c
Vsevolod Scovitin <scovitin.vsevolod@gmail.com>
2020-06-01 13:57:56 +0300

    MHWC-47 UX II recommendations home page

--- docroot/sites/all/themes/hwc_frontend/templates/block--partners.tpl.php
+++ docroot/sites/all/themes/hwc_frontend/templates/block--partners.tpl.php
@@ -1,24 +1,11 @@
 <?php
-/**
- * @file
- * Returns the HTML for a block.
- *
- * Complete documentation for this file is available online.
- * @see https://drupal.org/node/1728246
- *
- */
-?>
-<?php
 $translated = osha_tmgmt_literal_get_translation($title);
 ?>
-<div id="<?php print $block_html_id; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
-
-  <?php print render($title_prefix); ?>
-  <?php if ($translated): ?>
-    <h2<?php print $title_attributes; ?>><span><?php print $translated; ?></span></h2>
-  <?php endif; ?>
-  <?php print render($title_suffix); ?>
-
-  <?php print $content; ?>
-
-</div>
+<section class="slider--video--section">
+    <div class="slider--video--block row">
+        <div class="slider--video--items">
+            <h2 class="block-title"><?php print $translated; ?></h2>
+          <?php print $content; ?>
+        </div>
+    </div>
+</section>
