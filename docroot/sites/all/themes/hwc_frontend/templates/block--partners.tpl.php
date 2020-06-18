3bb5e4de26b7dcd4d2a054b6ec55414ca11dcdd8
Vsevolod Scovitin <scovitin.vsevolod@gmail.com>
2020-06-18 08:30:17 +0300

    MHWC-47 UX II recommendations home page

--- docroot/sites/all/themes/hwc_frontend/templates/block--partners.tpl.php
+++ docroot/sites/all/themes/hwc_frontend/templates/block--partners.tpl.php
@@ -4,7 +4,7 @@ $translated = osha_tmgmt_literal_get_translation($title);
 <section class="slider--video--section container">
     <div class="slider--video--block row">
         <div class="slider--video--items">
-            <h2 class="block-title"><?php print $translated; ?></h2>
+            <h2 class="block-title"><?php print l($translated, 'campaign-partners/official-campaign-partners'); ?></h2>
           <?php print $content; ?>
         </div>
     </div>
