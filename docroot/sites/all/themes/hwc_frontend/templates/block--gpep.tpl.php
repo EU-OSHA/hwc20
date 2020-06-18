721bf212a2b17ed5c952b646c9625d9da369dbac
Vsevolod Scovitin <scovitin.vsevolod@gmail.com>
2020-06-18 07:05:45 +0300

    MHWC-109 Add link to go back to My private workbench

--- docroot/sites/all/themes/hwc_frontend/templates/block--gpep.tpl.php
+++ docroot/sites/all/themes/hwc_frontend/templates/block--gpep.tpl.php
@@ -13,10 +13,11 @@ $translated = osha_tmgmt_literal_get_translation($title);
 ?>
 <?php
 $link_href = hwc_partner_get_user_partner_node_url();
-if ($link_href) {
-  $back_text = '<img alt="back page" src="/' . THEME_IMAGES_PATH . '/pag-back.png"><span>' . t('Back to Private Zone') . '</span>';
-  echo '<div class="back-links">' . l($back_text, $link_href, ['html' => TRUE]) . '</div>';
+if (!$link_href) {
+  $link_href = 'private_workbench';
 }
+$back_text = '<img alt="back page" src="/' . THEME_IMAGES_PATH . '/pag-back.png"><span>' . t('Back to Private Zone') . '</span>';
+echo '<div class="back-links">' . l($back_text, $link_href, ['html' => TRUE]) . '</div>';
 ?>
 <div id="<?php print $block_html_id; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
   <?php print render($title_prefix); ?>
