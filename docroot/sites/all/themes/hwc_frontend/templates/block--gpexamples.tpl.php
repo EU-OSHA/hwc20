ee8abd5de89b98fbd38fdd6d559d03a3199c8cfa
Vsevolod Scovitin <scovitin.vsevolod@gmail.com>
2020-06-24 17:50:04 +0300

    REL-1797 staging release

--- docroot/sites/all/themes/hwc_frontend/templates/block--gpexamples.tpl.php
+++ docroot/sites/all/themes/hwc_frontend/templates/block--gpexamples.tpl.php
@@ -2,14 +2,14 @@
 $translated = osha_tmgmt_literal_get_translation('Good practice examples');
 ?>
 <div id="<?php print $block_html_id; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
-<section class="slider--video--section">
+    <section class="slider--video--section">
       <?php print render($title_prefix); ?>
       <?php print render($title_suffix); ?>
-  <div class="slider--video--block row">
-    <div class="slider--video--items">
-      <h2 class="block-title"><?php print $translated; ?></h2>
-      <?php print $content; ?>
-    </div>
-  </div>
-</section>
+        <div class="slider--video--block row">
+            <div class="slider--video--items">
+                <h2 class="block-title"><?php print $translated; ?></h2>
+              <?php print $content; ?>
+            </div>
+        </div>
+    </section>
 </div>
--- docroot/sites/all/themes/hwc_frontend/templates/block--partners.tpl.php
+++ docroot/sites/all/themes/hwc_frontend/templates/block--partners.tpl.php
@@ -2,14 +2,14 @@
 $translated = osha_tmgmt_literal_get_translation($title);
 ?>
 <div id="<?php print $block_html_id; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
-<section class="slider--video--section container">
+    <section class="slider--video--section container">
       <?php print render($title_prefix); ?>
       <?php print render($title_suffix); ?>
-    <div class="slider--video--block row">
-        <div class="slider--video--items">
+        <div class="slider--video--block row">
+            <div class="slider--video--items">
                 <h2 class="block-title"><span><?php print l($translated, 'campaign-partners/official-campaign-partners'); ?></span></h2>
-          <?php print $content; ?>
+              <?php print $content; ?>
+            </div>
         </div>
-    </div>
-</section>
+    </section>
 </div>
